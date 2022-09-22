137bdb5f83933db8ea145a3076f4ed5638220d85
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-22 18:54:18 -0400

    page setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hey
+      <div>
+        <h1>Budgets</h1>
+        <button>add budget</button>
+        <button>add expense</button>
+        <div>
+          <span>Total</span>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>title</h1>
+          <span>min/max</span>
+        </div>
+        <div>progress bar</div>
+        <div>
+          <button>add expense</button>
+          <button> view expenses</button>
+        </div>
+      </div>
     </div>
   );
 }
