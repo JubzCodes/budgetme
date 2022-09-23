b320a2b158acc85006d334456a69e0d35c355dd2
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-23 12:01:33 -0400

    add states & getexpenses function

--- src/BudgetContext.js
+++ src/BudgetContext.js
@@ -1,5 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import React from "react";
+import { nanoid } from "nanoid";
+
 
 const BudgetsContext = React.createContext();
 
@@ -8,6 +10,18 @@ export const useBudgets  = () => {
 }
 
 export const BudgetsProvider = ({ children }) => {
+
+// BUGETS & EXPENSE STATE
+  const [budgets, setBudgets] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+
+//DISPLAY SELECTED EXPENSES
+  const getExpenses = (budgetId) => {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  }
+
+
+
   return (
     <BudgetsContext.Provider value={{}}>
       {children}
