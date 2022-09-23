6b8f15dd23c2ad47c12ce8388b48a37b2fbee1dd
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-09-23 10:38:24 -0400

    add content for cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
-import { Stack, Button } from '@mui/material/';
+import { Stack, Button, Card, CardContent, CardActions, Typography } from '@mui/material/';
+import { styled } from "@mui/material/styles";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+
+
 
 function App() {
+
+  const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 10,
+    borderRadius: 5,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor:
+        theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 5,
+      backgroundColor: theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
+    },
+  }));
+
+
+
+
   return (
     <div className="App">
       <div className="hero">
@@ -12,20 +35,95 @@ function App() {
             <Button variant="contained">Add Expense</Button>
           </Stack>
         </div>
-          <div>
-            <span>Total (donut chart)</span>
-          </div>
+        <div>
+          <span>Total (donut chart)</span>
+        </div>
       </div>
-      <div>
-        <div>
-          <h1>title</h1>
-          <span>min/max</span>
-        </div>
-        <div>progress bar</div>
-        <div>
-          <button>add expense</button>
-          <button> view expenses</button>
-        </div>
+      <div className="cards-wrap">
+        <Card className="card" sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <div className="card-title">
+              <Typography gutterBottom variant="h5" component="div">
+                Title
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Min/Max
+              </Typography>
+            </div>
+            <BorderLinearProgress variant="determinate" value={50} />
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="outlined">
+              Add Expense
+            </Button>
+            <Button size="small" variant="outlined">
+              View Expenses
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <div className="card-title">
+              <Typography gutterBottom variant="h5" component="div">
+                Title
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Min/Max
+              </Typography>
+            </div>
+            <BorderLinearProgress variant="determinate" value={50} />
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="outlined">
+              Add Expense
+            </Button>
+            <Button size="small" variant="outlined">
+              View Expenses
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <div className="card-title">
+              <Typography gutterBottom variant="h5" component="div">
+                Title
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Min/Max
+              </Typography>
+            </div>
+            <BorderLinearProgress variant="determinate" value={50} />
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="outlined">
+              Add Expense
+            </Button>
+            <Button size="small" variant="outlined">
+              View Expenses
+            </Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <div className="card-title">
+              <Typography gutterBottom variant="h5" component="div">
+                Title
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Min/Max
+              </Typography>
+            </div>
+            <BorderLinearProgress variant="determinate" value={50} />
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="outlined">
+              Add Expense
+            </Button>
+            <Button size="small" variant="outlined">
+              View Expenses
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     </div>
   );
